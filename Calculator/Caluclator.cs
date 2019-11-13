3910bd0e467964fcf6b2c364b920fd2c61f6d514
elbec <elbe.christopher@gmail.com>
2019-11-13 13:18:47 +0100

    Projektdateien hinzufügen.

---
+++ Calculator/Caluclator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorNS
+{
+    public class Calculator
+    {
+        public double add(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+        public double multiply(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        public double divide(double value1, double value2)
+        {
+            return value1 / value2;
+        }
+    }
+}
---
+++ CalculatorTests/UnitTestsCalculatorClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using CalculatorNS;
+
+namespace CalculatorNSTests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void TestAddFunction()
+        {
+            // Arrange
+            double value1 = 4;
+            double value2 = 10;
+            double expectedValue = 14;
+            Calculator calc = new Calculator();
+
+            // Act
+            double result = calc.add(value1, value2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result,"Add Function not working correct");
+        }
+
+        [TestMethod]
+        public void TestMultiplyFunction()
+        {
+            // Arrange
+            double value1 = 4;
+            double value2 = 10;
+            double expectedValue = 40;
+            Calculator calc = new Calculator();
+
+            // Act
+            double result = calc.multiply(value1, value2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result, "Multiply Function not working correct");
+        }
+
+        [TestMethod]
+        public void TestDivideFunction()
+        {
+            // Arrange
+            double value1 = 20;
+            double value2 = 4;
+            double expectedValue = 5;
+            Calculator calc = new Calculator();
+
+            // Act
+            double result = calc.divide(value1, value2);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result, "Multiply Function not working correct");
+        }
+    }
+}
